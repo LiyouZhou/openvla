b66d2b8515f2bb8e57cdb78ce8593bb040de1253
Liyou Zhou <liyou.zhou@outlook.com>
2025-05-25 01:07:50 +0100

    Add more representitive fine tune command

--- fine_tune_mikasa.sh
+++ fine_tune_mikasa.sh
@@ -1,18 +1,23 @@
+#!/bin/bash
+
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
 torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py \
-  --vla_path openvla/openvla-7b \
+  --vla_path ./logs/mikasa_baseline/openvla-7b+mikasa_baseline_mix+b16+lr-5e-05+lora-r16+dropout-0.1--mikasa_baseline_mix--image_aug--25-05-16_19-21-23--10000_chkpt/ \
   --data_root_dir /home/liyouzhou/tensorflow_datasets/ \
-  --dataset_name mikasa_robo_baseline_tfds \
+  --dataset_name mikasa_baseline_mix \
   --run_root_dir ~/study/openvla/logs \
-  --batch_size 3 \
+  --batch_size 2 \
   --learning_rate 5e-4 \
   --max_steps 50005 \
   --save_latest_checkpoint_only False \
   --image_aug True \
   --lora_rank 8 \
-  --save_steps 30000 \
+  --save_steps 10000 \
   --wandb_entity "leothemagnificent-university-of-cambridge" \
-  --wandb_project "openvla-mikasa-baseline" \
-  --run_id_note mikasa_robo_baseline_tfds \
-  --grad_accumulation_steps 2
+  --wandb_project "openvla-mikasa" \
+  --run_id_note mikasa_baseline_mix \
+  --grad_accumulation_steps 1 \
+  --val_frequency 10 \
+  --val_test_num_batches 20 \
+  --start_step 10000
