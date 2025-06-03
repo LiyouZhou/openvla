bf1803ec85663efc37e1940006ed3547b532da7c
Liyou Zhou <liyou.zhou@outlook.com>
2025-06-03 15:20:40 +0100

    Support parallel environments

--- experiments/robot/mikasa/run_mikasa_eval.py
+++ experiments/robot/mikasa/run_mikasa_eval.py
@@ -36,6 +36,13 @@ import gymnasium as gym
 from mikasa_robo_suite.utils.wrappers import StateOnlyTensorToDictWrapper
 from mikasa_robo_suite.dataset_collectors.get_mikasa_robo_datasets import env_info
 
+from prismatic.models.backbones.llm.prompting.base_prompter import PurePromptBuilder
+from prismatic.util.data_utils import PaddedCollatorForActionPrediction
+from prismatic.vla.action_tokenizer import ActionTokenizer
+from prismatic.vla.datasets.datasets import RLDSBatchTransform
+
+from transformers.modeling_outputs import CausalLMOutputWithPast
+
 # Append current directory so that interpreter can find experiments.robot
 sys.path.append("../..")
 # from experiments.robot.libero.libero_utils import (
@@ -142,8 +149,11 @@ class GenerateConfig:
 
     seed: int = 7                                    # Random Seed (for reproducibility)
 
-    # [OpenVLA] Set action un-normalization key
-    unnorm_key: str = "mikasa_robo_baseline_tfds"
+    unnorm_key: str = "mikasa_robo_baseline_tfds"    # [OpenVLA] Set action un-normalization key
+    baseline_prompt: bool = False                    # Whether to use the baseline prompt with priviliged information
+
+    num_envs: int = 4                                # Number of environments to run in parallel (for multi-agent tasks)
+    #################################################################################################################
     # fmt: on
 
 
@@ -174,6 +184,16 @@ def eval_mikasa(cfg: GenerateConfig) -> None:
     processor = None
     if cfg.model_family == "openvla":
         processor = get_processor(cfg)
+        action_tokenizer = ActionTokenizer(processor.tokenizer)
+        batch_transform = RLDSBatchTransform(
+            action_tokenizer,
+            processor.tokenizer,
+            image_transform=processor.image_processor.apply_transform,
+            prompt_builder_fn=PurePromptBuilder,
+        )
+        collator = PaddedCollatorForActionPrediction(
+            processor.tokenizer.model_max_length, processor.tokenizer.pad_token_id, padding_side="right"
+        )
 
     # Initialize local logging
     run_id = f"EVAL-{cfg.task_suite_name}-{cfg.model_family}-{DATE_TIME}"
@@ -217,7 +237,7 @@ def eval_mikasa(cfg: GenerateConfig) -> None:
         # env, task_description = get_libero_env(task, cfg.model_family, resolution=256)
         env_name = "RememberColor3-v0"
         # seed = 42
-        num_envs = 1
+        num_envs = cfg.num_envs
         env_kwargs_rgb = dict(
             num_envs=num_envs,
             obs_mode="rgb",
@@ -237,101 +257,136 @@ def eval_mikasa(cfg: GenerateConfig) -> None:
         # Start episodes
         task_episodes, task_successes = 0, 0
         dist_to_target = []
-        for episode_idx in tqdm.tqdm(range(cfg.num_trials_per_task)):
+        all_rewards = []
+        for episode_idx in tqdm.trange(0, cfg.num_trials_per_task, num_envs):
             # Reset environment
             obs, info = env.reset()
 
-            oracle_info = int(info["oracle_info"].cpu())
-            color = ["red", "green", "blue"][oracle_info]
-            task_description = f"Touch the {color} cube"
+            oracle_info = [int(x) for x in info["oracle_info"].cpu()]
+            colors = [["red", "green", "blue"][x] for x in oracle_info]
+            if cfg.baseline_prompt:
+                prompts = [f"Touch the {color} cube" for color in colors]
+            else:
+                prompts = [
+                    "Memorize the the colors of the cube shown on the table, and then touch the same coloured cube out of all the cubes."
+                ] * len(colors)
 
-            print(f"\nTask: {task_description}")
-            log_file.write(f"\nTask: {task_description}\n")
+            log_file.write(f"\nTask: {prompts}\n")
 
             # Set initial states
             # obs = env.set_init_state(initial_states[episode_idx])
 
             # Setup
             t = 0
-            replay_images = []
-            if cfg.task_suite_name == "libero_spatial":
-                max_steps = 220  # longest training demo has 193 steps
-            elif cfg.task_suite_name == "libero_object":
-                max_steps = 280  # longest training demo has 254 steps
-            elif cfg.task_suite_name == "libero_goal":
-                max_steps = 300  # longest training demo has 270 steps
-            elif cfg.task_suite_name == "libero_10":
-                max_steps = 520  # longest training demo has 505 steps
-            elif cfg.task_suite_name == "libero_90":
-                max_steps = 400  # longest training demo has 373 steps
-            else:
-                max_steps = 500  # default max steps
+            replay_images = [[] for _ in range(num_envs)]
+            max_steps = 500  # default max steps
 
             print(f"Starting episode {task_episodes+1}...")
             log_file.write(f"Starting episode {task_episodes+1}...\n")
-            terminated = False
+            terminated_flags = np.array([False] * num_envs)
+            truncated_flags = np.array([False] * num_envs)
+            success_flags = np.array([False] * num_envs)
+            final_rewards = np.zeros(num_envs)
+            final_distances = np.zeros(num_envs)
+
             while t < max_steps + cfg.num_steps_wait:
                 # try:
                 # IMPORTANT: Do nothing for the first few timesteps because the simulator drops objects
                 # and we need to wait for them to fall
                 if t < cfg.num_steps_wait:
                     action = env.action_space.sample()
+                    action = np.zeros(action.shape)
                     obs, reward, terminated, truncated, info = env.step(action)
                     t += 1
                     continue
 
                 # Get preprocessed image
-                # img = get_libero_image(obs, resize_size)
-                img = obs["sensor_data"]["base_camera"]["rgb"][0]
+                img = obs["sensor_data"]["base_camera"]["rgb"]
                 img = img.cpu().numpy()
 
                 # Save preprocessed image for replay video
-                replay_images.append(img)
-
+                for i in range(num_envs):
+                    replay_images[i].append(img[i])
                 # Prepare observations dict
                 # Note: OpenVLA does not take proprio state as input
-                observation = {
-                    "full_image": img,
-                    # "state": np.concatenate(
-                    #     (obs["robot0_eef_pos"], quat2axisangle(obs["robot0_eef_quat"]), obs["robot0_gripper_qpos"])
-                    # ),
-                }
+                samples = [
+                    {
+                        "dataset_name": cfg.unnorm_key,
+                        "action": np.zeros((1, 7)),  # Dummy action
+                        "observation": {"image_primary": img[i : i + 1]},
+                        "task": {
+                            "language_instruction": prompts[i].encode("utf-8"),
+                        },
+                    }
+                    for i in range(num_envs)
+                ]
 
                 # Query model to get action
-                action = get_action(
-                    cfg,
-                    model,
-                    observation,
-                    task_description,
-                    processor=processor,
-                )
+                # action = get_action(
+                #     cfg,
+                #     model,
+                #     observation,
+                #     prompt,
+                #     processor=processor,
+                # )
                 # print(f"Action: {action}")
+                transformed_samples = [batch_transform(sample) for sample in samples]
+                batch = collator(transformed_samples)
+                model.eval()
+                with torch.no_grad():
+                    with torch.autocast("cuda", dtype=torch.bfloat16):
+                        output: CausalLMOutputWithPast = model(
+                            input_ids=batch["input_ids"].to("cuda"),
+                            attention_mask=batch["attention_mask"].to("cuda"),
+                            pixel_values=batch["pixel_values"].to(torch.bfloat16).to("cuda"),
+                            labels=batch["labels"],
+                        )
 
-                # Normalize gripper action [0,1] -> [-1,+1] because the environment expects the latter
-                action = normalize_gripper_action(action, binarize=True)
+                action_logits = output.logits[:, model.vision_backbone.featurizer.patch_embed.num_patches : -1]
+                action_preds = action_logits.argmax(dim=2)
+                continuous_actions_pred = action_tokenizer.decode_token_ids_to_actions(
+                    action_preds[:, -8:-1].cpu().numpy()
+                )
 
-                # [OpenVLA] The dataloader flips the sign of the gripper action to align with other datasets
-                # (0 = close, 1 = open), so flip it back (-1 = open, +1 = close) before executing the action
-                if cfg.model_family == "openvla":
-                    action = invert_gripper_action(action)
+                action_norm_stats = model.get_action_stats(cfg.unnorm_key)
+                action_high, action_low = np.array(action_norm_stats["q99"]), np.array(action_norm_stats["q01"])
+                mask = action_norm_stats.get("mask", np.ones_like(action_norm_stats["q01"], dtype=bool))
+
+                # print("batch['labels']", batch["labels"].shape)
+                # print(f"output logits: {output.logits.shape}")
+                # print(f"action logits: {action_logits.shape}")
+                # print(f"action preds: {action_preds.shape}")
+                # print(f"continuous actions pred: {continuous_actions_pred.shape}")
+
+                actions = np.where(
+                    mask,
+                    0.5 * (continuous_actions_pred + 1) * (action_high - action_low) + action_low,
+                    continuous_actions_pred,
+                )
+
+                # print(f"Action: {actions.shape}")
 
                 # Execute action in environment
                 # print(f"Action: {type(action)} {action.shape} {action}")
                 # action = np.append(action, action[-1])
-                action = torch.from_numpy(action)
-                action = action * 10
-                action = torch.stack([action] * num_envs)
-                obs, reward, terminated, truncated, info = env.step(action)
-                terminated = True if terminated[0].cpu() else False
-                truncated = True if truncated[0].cpu() else False
-                # print(reward, info)
+                actions = torch.from_numpy(actions)
+                actions = actions * 10
+                obs, reward, terminated, truncated, info = env.step(actions)
 
-                if info["success"] == 1:
-                    task_successes += 1
-                    total_successes += 1
-                    break
+                for i in range(num_envs):
+                    if terminated[i].cpu().numpy():
+                        terminated_flags[i] = True
+                        final_rewards[i] = reward[i].cpu().numpy()
+                        final_distances[i] = info["reward_dict"]["tcp_to_obj_dist"][i].cpu().numpy()
+                    if truncated[i].cpu().numpy():
+                        truncated_flags[i] = True
+                        final_rewards[i] = reward[i].cpu().numpy()
+                        final_distances[i] = info["reward_dict"]["tcp_to_obj_dist"][i].cpu().numpy()
+                    if info["success"][i].cpu().numpy():
+                        success_flags[i] = True
 
-                if terminated or truncated:
+                if all(terminated_flags | truncated_flags):
+                    # print("All environments terminated or truncated, ending episode.")
                     break
                 t += 1
 
@@ -340,24 +395,35 @@ def eval_mikasa(cfg: GenerateConfig) -> None:
                 #     log_file.write(f"Caught exception: {e}\n")
                 #     break
 
-            task_episodes += 1
-            total_episodes += 1
+            task_successes += np.sum(success_flags)
+            total_successes += np.sum(success_flags)
 
             # Save a replay video of the episode
-            mp4_path = save_rollout_video(
-                replay_images, total_episodes, success=terminated, task_description=task_description, log_file=log_file
-            )
+            for i in range(num_envs):
+                task_episodes += 1
+                total_episodes += 1
 
-            dist_to_target.append(info["reward_dict"]["tcp_to_obj_dist"].cpu().numpy())
-            if cfg.use_wandb:
-                wandb.log(
-                    {
-                        f"rollout_video/{task_description}": wandb.Video(mp4_path, format="mp4"),
-                        f"sucess": info["success"].cpu(),
-                        f"distance_to_target": info["reward_dict"]["tcp_to_obj_dist"],
-                        f"episode_idx": episode_idx,
-                    }
+                mp4_path = save_rollout_video(
+                    replay_images[i],
+                    total_episodes,
+                    success=terminated_flags[i],
+                    task_description=env_name,
+                    log_file=log_file,
                 )
+
+                if cfg.use_wandb:
+                    wandb.log(
+                        {
+                            f"rollout_video/{env_name}/{colors[i]}": wandb.Video(mp4_path, format="mp4"),
+                            f"sucess": success_flags[i],
+                            f"distance_to_target": final_distances[i],
+                            f"reward": final_rewards[i],
+                            f"episode_idx": task_episodes - 1,
+                        }
+                    )
+                
+                dist_to_target.append(final_distances[i])
+                all_rewards.append(final_rewards[i])
 
             # Log current results
             print(f"Success: {terminated}")
@@ -370,19 +436,24 @@ def eval_mikasa(cfg: GenerateConfig) -> None:
 
         # Log final results
         avg_dist_to_target = np.mean(dist_to_target)
+        average_reward = np.mean(all_rewards)
+
         print(f"Current task success rate: {float(task_successes) / float(task_episodes)}")
         print(f"Current total success rate: {float(total_successes) / float(total_episodes)}")
         print(f"Average Distance to target: {avg_dist_to_target}")
+        print(f"Average Reward: {average_reward}")
         log_file.write(f"Current task success rate: {float(task_successes) / float(task_episodes)}\n")
         log_file.write(f"Current total success rate: {float(total_successes) / float(total_episodes)}\n")
         log_file.write(f"Average Distance to target: {avg_dist_to_target}\n")
+        log_file.write(f"Average Reward: {average_reward}\n")
         log_file.flush()
         if cfg.use_wandb:
             wandb.log(
                 {
-                    f"{task_description}/success_rate": float(task_successes) / float(task_episodes),
-                    f"{task_description}/num_episodes": task_episodes,
-                    f"{task_description}/average_distance": avg_dist_to_target,
+                    f"{env_name}/success_rate": float(task_successes) / float(task_episodes),
+                    f"{env_name}/num_episodes": task_episodes,
+                    f"{env_name}/average_distance": avg_dist_to_target,
+                    f"{env_name}/average_reward": average_reward,
                 }
             )
 
