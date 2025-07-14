211f8549a7254d20c4ab0c9041f9b8228a2aa57c
Liyou Zhou <liyou.zhou@outlook.com>
2025-07-14 23:28:30 +0100

    Support the mikasa all dataset

--- prismatic/vla/datasets/rlds/oxe/configs.py
+++ prismatic/vla/datasets/rlds/oxe/configs.py
@@ -677,6 +677,13 @@ OXE_DATASET_CONFIGS = {
         "state_encoding": StateEncoding.POS_EULER,
         "action_encoding": ActionEncoding.EEF_POS,
     },
+    "mikasa_robo_tfds_all": {
+        "image_obs_keys": {"primary": "image", "secondary": None, "wrist": None},
+        "depth_obs_keys": {"primary": None, "secondary": None, "wrist": None},
+        "state_obs_keys": ["state"],
+        "state_encoding": StateEncoding.POS_EULER,
+        "action_encoding": ActionEncoding.EEF_POS,
+    },
 }
 
 MIKASA_ROBO_TFDS_TASKS = [
